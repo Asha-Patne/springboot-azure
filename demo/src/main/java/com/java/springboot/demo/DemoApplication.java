d1eb2da02e1717e123beb02c5b8e34587a44ccf2
unknown <asha.patne@gmail.com>
2023-07-02 21:03:32 +0530

    my second commit

--- demo/src/main/java/com/java/springboot/demo/DemoApplication.java
+++ demo/src/main/java/com/java/springboot/demo/DemoApplication.java
@@ -19,7 +19,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		log.info("Welcome to Spring Boot Application with Swagger...!!!!");
+		log.info("Welcome to Spring Boot Application with Swagger and deployed to Azure...!!!!");
 	}
 
 }
